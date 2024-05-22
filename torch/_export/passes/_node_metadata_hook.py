2fb4de58c0a41d857b9fd8c221eb9ff3968d8ac6
Sheng Fu <shengfu@meta.com>
2024-05-22 00:16:16 -0700

    Support multiple hooks for creating/erasing GraphModule node (#126671)
    Summary:
    GraphModule supports a call back function (_create_node_hook) for creating a node, it is mainly for meta data export.
    
    In my prototype(https://www.internalfb.com/diff/D56974659), I also use the call back functions for creating/erasing a node to observe the change in every pass of graph transform.
    
    So it means GraphModule may have multiple observers listening to the change of the graph. This DIFF is to support multiple hooks for both creating and erasing nodes.
    
    I am going to have a few follow up DIFFs to support GraphTransformObserver after this DIFF is landed.
    
    
    Test Plan:
    buck2 test mode/dev-nosan //caffe2/test:test_export
    buck2 test mode/dev-sand caffe2/test:fx -- fx.test_fx_node_hook.TestFXNodeHook
    
    Differential Revision: D57469157

--- torch/_export/passes/_node_metadata_hook.py
+++ torch/_export/passes/_node_metadata_hook.py
@@ -1,4 +1,7 @@
+import contextlib
+
 import torch
+from torch.fx.graph_module import GraphModule
 
 
 def _node_metadata_hook(node: torch.fx.Node, stack_trace: str) -> None:
@@ -7,7 +10,7 @@ def _node_metadata_hook(node: torch.fx.Node, stack_trace: str) -> None:
     pass using graph.create_node. An example of how to use it:
 
     ```
-    with gm.graph._set_create_node_hook(
+    with _set_node_metadata_hook(gm,
         functools.partial(_node_metadata_hook, stack_trace="file")
     ):
         pass(gm)
@@ -42,3 +45,24 @@ def _node_metadata_hook(node: torch.fx.Node, stack_trace: str) -> None:
         f"{node.target.__name__}_0",
         f"{node.target.__class__.__name__}.{node.target.__name__}",
     )
+
+
+@contextlib.contextmanager
+def _set_node_metadata_hook(gm: torch.fx.GraphModule, f):
+    """
+    Takes a callable which will be called after we create a new node. The
+    callable takes the newly created node as input and returns None.
+    """
+    assert callable(f), "node_metadata_hook must be a callable."
+
+    # Add the hook to all submodules
+    for m in gm.modules():
+        if isinstance(m, GraphModule):
+            m._register_create_node_hook(f)
+    try:
+        yield
+    finally:
+        # Restore hook for all submodules
+        for m in gm.modules():
+            if isinstance(m, GraphModule):
+                m._unregister_create_node_hook(f)
